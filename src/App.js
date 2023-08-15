0c257da33326d5fb2f66af841b65ecfae7dbc9b4
Diego <doandtvp@gmail.com>
2023-08-15 10:31:55 +0700

    update code

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
 import React from 'react'
 import UploadImage from './component/UploadImage'
 import Preview from './component/Preview'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { HashRouter, Route, Routes } from 'react-router-dom'
 
 function App() {
   return (
     <div className='container'>
-      <BrowserRouter>
+      <HashRouter>
         <Routes>
           <Route path="/" element={<UploadImage/>} />
           <Route path="/preview" element={<Preview/>} />
           <Route path="*" element={<p>Sorry, nothing here</p>} />
         </Routes>
-      </BrowserRouter>
+      </HashRouter>
     </div>
   )
 }
